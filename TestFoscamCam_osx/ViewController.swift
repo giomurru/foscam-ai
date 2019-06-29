2b51e6dc7a6ccadd07c940b9faec7523ac6732c0
Giovanni Murru <giovanni.murru@gmail.com>
2019-06-29 16:06:13 +0200

    Use LoginConstants in osx ViewController

--- TestFoscamCam_osx/ViewController.swift
+++ TestFoscamCam_osx/ViewController.swift
@@ -68,7 +68,14 @@ class ViewController: FaceDetectorViewController, MJPEGLibDelegate, FaceDetector
         }
         // Do any additional setup after loading the view.
         // Set the ImageView to the stream object
-        cameraController = FoscamControl(with: "192.168.1.104", user: "admin", password: "45gnAX.%2F114", streamDelegate: self)
+//        Note: to use FoscamControl you have to create a struct called LoginConstants with your credentials such as the one in the example below.
+//        Example:
+//        struct LoginConstants {
+//            static let domain = "192.168.1.104"
+//            static let user = "admin"
+//            static let pwd = "password"
+//        }
+        cameraController = FoscamControl(with: LoginConstants.domain, user: LoginConstants.user, password: LoginConstants.pwd, streamDelegate: self)
         self.captureDeviceResolution = CGSize(width: 640, height: 480)
         self.prepareVisionRequest()
         cameraController.startStreaming()
